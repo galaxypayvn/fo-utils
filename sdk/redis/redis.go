83320a88a05fb8404a1faff323b9a618ae889e92
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2024-04-19 09:02:03 +0700

    Change sinagure of set and get hash

--- sdk/redis/redis.go
+++ sdk/redis/redis.go
@@ -35,8 +35,8 @@ type IRedisRepo interface {
 	Del(ctx context.Context, key string) error
 	SetKey(ctx context.Context, key string, value interface{}, expire time.Duration) (err error)
 	GetKey(ctx context.Context, key string) (value string, err error)
-	SetHash(ctx context.Context, key string, value interface{}, expire time.Duration) (err error)
-	GetHash(ctx context.Context, key string) (res map[string]string, err error)
+	SetHash(ctx context.Context, key string, value interface{}) (err error)
+	GetHash(ctx context.Context, key string, res any) (err error)
 	CheckExist(ctx context.Context, key string) (res bool, err error)
 	GetHashByKey(ctx context.Context, key string, field string) (res string, err error)
 }
@@ -90,25 +90,22 @@ func (r *RedisRepo) GetKey(ctx context.Context, key string) (value string, err e
 	return value, err
 }
 
-func (r *RedisRepo) SetHash(ctx context.Context, key string, value interface{}, expire time.Duration) (err error) {
+func (r *RedisRepo) SetHash(ctx context.Context, key string, value interface{}) (err error) {
 	err = r.RDB.HSet(ctx, key, value).Err()
 	if err != nil {
 		return err
 	}
-	err = r.RDB.Expire(ctx, key, expire).Err()
-	if err != nil {
-		return err
-	}
+
 	return nil
 }
 
-func (r *RedisRepo) GetHash(ctx context.Context, key string) (res map[string]string, err error) {
-	res, err = r.RDB.HGetAll(ctx, key).Result()
+func (r *RedisRepo) GetHash(ctx context.Context, key string, res any) error {
+	err := r.RDB.HGetAll(ctx, key).Scan(res)
 	if err != nil {
-		return res, err
+		return err
 	}
 
-	return res, nil
+	return nil
 }
 
 func (r *RedisRepo) GetHashByKey(ctx context.Context, key string, field string) (res string, err error) {
