5c6faf58a09ad38318d02c57013b56959be3ae47
trunghq234 <trung.huynh.quang@sobanhang.com>
2024-04-23 10:21:03 +0700

    fix

--- valid/valid_pointer.go
+++ valid/valid_pointer.go
@@ -1,8 +1,49 @@
 package valid
 
 import (
+	"github.com/google/uuid"
 	"time"
 )
+
+func BoolPointer(b bool) *bool {
+	return &b
+}
+
+func StringPointer(s string) *string {
+	return &s
+}
+
+func IntPointer(i int) *int {
+	return &i
+}
+
+func Int32Pointer(i int32) *int32 {
+	return &i
+}
+
+func Int64Pointer(i int64) *int64 {
+	return &i
+}
+
+func Float64Pointer(i float64) *float64 {
+	return &i
+}
+
+func Float32Pointer(i float32) *float32 {
+	return &i
+}
+
+func DayTimePointer(i time.Time) *time.Time {
+	if i.IsZero() {
+		return nil
+	} else {
+		return &i
+	}
+}
+
+func UUIDPointer(i uuid.UUID) *uuid.UUID {
+	return &i
+}
 
 /*
 GetPointer returns a pointer to the input value of type T.
