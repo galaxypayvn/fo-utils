94d658b0c5dc0a288c7882160b8f80397f5a70df
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2024-06-29 21:15:06 +0700

    Add custom time type

--- utils/utfunc/copy.go
+++ utils/utfunc/copy.go
@@ -1,9 +1,34 @@
 package utfunc
 
-import "github.com/jinzhu/copier"
+import (
+	"time"
+
+	"code.finan.cc/finan-one-be/fo-utils/utils/customtype"
+	"github.com/jinzhu/copier"
+)
+
+var customTimeToTimeConverter = copier.TypeConverter{
+	SrcType: customtype.Time{},
+	DstType: time.Time{},
+	Fn: func(src interface{}) (interface{}, error) {
+		customTm, ok := src.(customtype.Time)
+		if !ok {
+			return time.Time{}, nil
+		}
+
+		return customTm.Time, nil
+	},
+}
 
 func CopyWhenUpdate(toValue any, fromValue any) error {
 	return copier.CopyWithOption(toValue, fromValue, copier.Option{
 		IgnoreEmpty: true,
+		Converters:  []copier.TypeConverter{customTimeToTimeConverter},
+	})
+}
+
+func CopyWhenInsert(toValue any, fromValue any) error {
+	return copier.CopyWithOption(toValue, fromValue, copier.Option{
+		Converters: []copier.TypeConverter{customTimeToTimeConverter},
 	})
 }
