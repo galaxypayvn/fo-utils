c1d1da4f42084e0414887bc5d52ebf1567119082
trunghq234 <trung.huynh.quang@sobanhang.com>
2024-08-15 16:17:25 +0700

    add func NormalizeEmail

--- utils/utmail/utmail.go
+++ utils/utmail/utmail.go
@@ -2,14 +2,47 @@ package utmail
 
 import (
 	"regexp"
+	"strings"
 )
 
 // ValidEmail is a function that validates if the input string is a valid email address.
 func ValidEmail(input string) bool {
 	pattern := `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
 	re := regexp.MustCompile(pattern)
-	if !re.MatchString(input) {
-		return false
+	return re.MatchString(input)
+}
+
+// NormalizeEmail normalizes an email address
+func NormalizeEmail(input string) string {
+	rulePlusOnly := regexp.MustCompile(`\+.*$`)
+	rulePlusAndDot := regexp.MustCompile(`\.|\+.*$`)
+
+	normalizableProviders := map[string]struct {
+		rule    *regexp.Regexp
+		aliasOf string
+	}{
+		"gmail.com":      {rule: rulePlusAndDot},
+		"googlemail.com": {rule: rulePlusAndDot, aliasOf: "gmail.com"},
+		"hotmail.com":    {rule: rulePlusOnly},
+		"live.com":       {rule: rulePlusAndDot},
+		"outlook.com":    {rule: rulePlusOnly},
 	}
-	return true
+
+	email := strings.ToLower(input)
+	emailParts := strings.SplitN(email, "@", 2)
+
+	if len(emailParts) != 2 {
+		return input
+	}
+
+	username, domain := emailParts[0], emailParts[1]
+
+	if provider, ok := normalizableProviders[domain]; ok {
+		username = provider.rule.ReplaceAllString(username, "")
+		if provider.aliasOf != "" {
+			domain = provider.aliasOf
+		}
+	}
+
+	return username + "@" + domain
 }
